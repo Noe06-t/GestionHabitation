204860a0e31aefa40aba445389c8a3af6ee6efac
Noe06-t <Offinobinoe@gmail.com>
2026-02-11 17:41:58 +0000

    Add authentication and profile management features
    - Implemented user registration, login, and password reset functionality.
    - Created profile management views for updating user information and password.
    - Added email verification and account deletion features.
    - Developed reusable Blade components for input fields, buttons, and modals.
    - Established routes for authentication and profile management.
    - Included tests for authentication, email verification, password reset, and profile updates.
    - Integrated Tailwind CSS for styling and responsive design.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\HabitantController;
-use App\Models\Habitant;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CertificatController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Routes pour les habitants
-Route::resource('habitants',HabitantController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Routes pour les certificats 
-Route::resource('certificats', CertificatController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
